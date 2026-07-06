8b512360454a786d5dfb5144061b83b7b26efb0b
Aishwarya S <aishwaryasuresh636@gmail.com>
2026-07-06 10:06:00 +0530

    auto-commit

--- student-course-portal--routing/src/app/pages/home/home.ts
+++ student-course-portal--routing/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,18 +8,22 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
 
   portalName = 'Student Course Portal';
-
   isPortalActive = true;
-
   message = '';
-
   searchTerm = '';
+
+  ngOnInit(): void {
+    console.log('HomeComponent initialized - courses loaded');
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+  }
 
   onEnrollClick() {
     this.message = 'Enrollment opened!';
   }
-
 }
