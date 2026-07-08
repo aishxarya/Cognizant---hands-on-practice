5ade19076fc65064c1f393632c52a021500dd33d
Aishwarya S <aishwaryasuresh636@gmail.com>
2026-07-08 22:00:34 +0530

    auto-commit

--- student-course-portal--routing/src/app/pages/course-list/course-list.ts
+++ student-course-portal--routing/src/app/pages/course-list/course-list.ts
@@ -20,7 +20,7 @@ courses = [
 selectedCourseId?: number;
 isLoading = true;
 ngOnInit() : void {
-  console.log("ngOnInit called")
+  console.log("ngOnInit called");
   setTimeout(() => {
     this.isLoading = false;
   }, 1500);
