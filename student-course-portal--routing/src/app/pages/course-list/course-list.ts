7d842e08833147e1b29be5f1b8305e45fb9af77b
Aishwarya S <aishwaryasuresh636@gmail.com>
2026-07-08 20:27:09 +0530

    auto-commit

--- student-course-portal--routing/src/app/pages/course-list/course-list.ts
+++ student-course-portal--routing/src/app/pages/course-list/course-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnI } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseCard } from '../../components/course-card/course-card';
 
